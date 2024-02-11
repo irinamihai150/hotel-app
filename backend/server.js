5d211c2100afc62e06b8de89d9f1be0266a7a9a9
irinamihai150 <irinamihai150@gmail.com>
2024-02-11 10:04:12 +0000

    text

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
 	)
 } else {
 	app.get("/", (req, res) => {
-		res.send("Api is running from docker :)")
+		res.send("Api is running from docker  image :)")
 	})
 }
 
